703102df190e62c683383e14d6167f07b4aba6c9
Shreyash0368 <114088740+Shreyash0368@users.noreply.github.com>
2024-08-21 09:40:04 +0530

    minor fix in modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="flex justify-center p-4">
-    <button
-      id="button"
-      data-modal-toggle="modal"
-      data-modal-target="modal"
-      type="button"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-    >
-      Show modal
-    </button>
-  </div>
-  <div class="m-3">
-    <p class="font-semibold text-center" v-if="displayText">
-      {{ displayText }}
-    </p>
+  <div>
+    <div class="flex justify-center p-4">
+      <button
+        id="button"
+        data-modal-toggle="modal"
+        data-modal-target="modal"
+        type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Show modal
+      </button>
+    </div>
+    <div class="m-3">
+      <p class="font-semibold text-center" v-if="displayText">
+        {{ displayText }}
+      </p>
+    </div>
   </div>
 
   <div
